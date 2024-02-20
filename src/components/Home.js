578903f4d818669333b75cb9c962a388af0e0c95
joanawuor <joan.obunge@learninglions.org>
2024-02-20 11:26:08 +0300

    deleted the comment

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,6 @@ const HeroSection = () => {
         <h1 className="hero-title">
           LET'S <br /> EMBRACE <br /> EQUITY
         </h1>
-        <h2>LEARNING LIONS INFORMATION COMMENT</h2>
         <button className="primary-button">DONATE NOW</button>
       </div>
     </div>
