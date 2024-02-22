4c6f4cf2d98763fc31779629ffe2bf9b0615866e
joanawuor <joan.obunge@learninglions.org>
2024-02-22 12:37:47 +0300

    Make change to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,39 @@
 import React from "react";
+import image from "../Assets/second-sec-image.png";
 
-const HeroSection = () => {
+const Home = () => {
   return (
-    <div className="hero-section">
-      <div className="hero-content">
-        <h1 className="hero-title">
-          LET'S <br /> EMBRACE <br /> EQUITY
-        </h1>
-        <button className="primary-button">DONATE NOW</button>
+    <div>
+      <div className="hero-section">
+        <div className="hero-content">
+          <h1 className="hero-title">
+            LET'S <br /> EMBRACE <br /> EQUITY
+          </h1>
+          <button className="primary-button">DONATE NOW</button>
+        </div>
       </div>
+      <div className="row">
+        <div className="column">
+      <div className="fundraising-section">
+        <img src={image} />
+      </div>
+      </div>
+      <div className="column">
+      <div className="fundraising-text">
+        <p>
+          Nawi" highlights the distressing issue of early marriages in Kenya,
+          depicting the challenges faced by young individuals thrust into
+          matrimony. The film explores the interconnection between early
+          marriages and education obstacles, showcasing the profound impact on
+          the lives of those involved. It calls for increased awareness and
+          policy changes to break this cycle, advocating for a future where
+          education becomes a key tool against early marriages.
+        </p>
+      </div>
+      </div>
+    </div>
     </div>
   );
 };
 
-export default HeroSection;
+export default Home;
