d2e6c89ecdfca654346f521962edca305c31770d
emf-jack <112486851+emf-jack@users.noreply.github.com>
2024-02-22 12:33:40 +0300

    add React Router for navigation between pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import Fundraising from "./components/Fundraising";
+import News from "./components/News";
+import Impact from "./components/Impact";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/fundraising" element={<Fundraising />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/impact" element={<Impact />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
