7cbc99c4912d9c5a1e5e7a363878eadc72f01302
emf-jack <112486851+emf-jack@users.noreply.github.com>
2024-02-22 11:58:16 +0300

    wip icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,44 @@
 import React from "react";
+const icon1 = require("../Assets/icons/Icon1.png");
 
-const HeroSection = () => {
+const Home = () => {
   return (
-    <div className="hero-section">
-      <div className="hero-content">
-        <h1 className="hero-title">
-          LET'S <br /> EMBRACE <br /> EQUITY
-        </h1>
-        <button className="primary-button">DONATE NOW</button>
+    <div>
+      <div className="hero-section">
+        <div className="hero-content">
+          <h1 className="hero-title">
+            LET'S <br /> EMBRACE <br /> EQUITY
+          </h1>
+          <button className="primary-button">DONATE NOW</button>
+        </div>
+      </div>
+      <div className="fundraising-section">
+        <div className="fundraising-content">
+          <h1 className="fundraising-title">FUNDRAISING CAMPAIGN OVERVIEW</h1>
+          <button className="primary-button">DONATE NOW</button>
+        </div>
+        <div className="fundraising-icons">
+          <div className="icon">
+            <img src={icon1}></img>
+            <img src={require("../Assets/icons/Icon1.png")}></img>
+            <p className="icon-title"></p>
+          </div>
+          <div className="icon">
+            <img></img>
+            <p className="icon-title"></p>
+          </div>
+          <div className="icon">
+            <img></img>
+            <p className="icon-title"></p>
+          </div>
+          <div className="icon">
+            <img></img>
+            <p className="icon-title"></p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default HeroSection;
+export default Home;
