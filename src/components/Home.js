643312a8e6dd836b00bcb143186895ca3a9ce4f9
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-02-23 19:48:53 +0300

    Added another image to Assets

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
-import image from "../Assets/second-sec-image.png";
+import image from "../Assets/secondSecImage.png";
+import Icon1 from "../Assets/icons/Icon1.png";
+import Icon2 from "../Assets/icons/Icon2.png";
+import Icon3 from "../Assets/icons/Icon3.png";
+import Icon4 from "../Assets/icons/Icon4.png";
 
 const Home = () => {
   return (
@@ -12,32 +16,65 @@ const Home = () => {
           <button className="primary-button">DONATE NOW</button>
         </div>
       </div>
-      <div className="fundraising-section">
-        <div className="fundraising-content">
-          <h1 className="fundraising-title">FUNDRAISING CAMPAIGN OVERVIEW</h1>
-          <button className="primary-button">DONATE NOW</button>
-        </div>
-      </div>
       <div className="row">
         <div className="column">
-      <div className="fundraising-section">
-        <img src={image} />
+          <div className="fundraising-section">
+            <img src={image} alt="Fundraising Image" />
+          </div>
+        </div>
+        <div className="column">
+          <div className="fundraising-text">
+            <p>
+              "Nawi" highlights the distressing issue of early marriages in
+              Kenya, depicting the challenges faced by young individuals thrust
+              into matrimony. The film explores the interconnection between
+              early marriages and education obstacles, showcasing the profound
+              impact on the lives of those involved. It calls for increased
+              awareness and policy changes to break this cycle, advocating for a
+              future where education becomes a key tool against early marriages.
+            </p>
+          </div>
+        </div>
       </div>
+      <div className="fundraising-overview">
+        <h2 className="fundraising-overview-h2">
+          FUNDRAISING CAMPAIGN OVERVIEW
+        </h2>
+        <div className="icons-container">
+          <div className="icon">
+            <img src={Icon1} alt="Icon 1" />
+            <p>
+              EDUCATION <br /> INITIATIVES
+            </p>
+            <p>lorem ipsum</p>
+          </div>
+          <div className="icon">
+            <img src={Icon2} alt="Icon 2" />
+            <p>
+              COMMUNITY <br /> RESILIENCE
+            </p>
+            <p>lorem ipsum</p>
+          </div>
+          <div className="icon">
+            <img src={Icon3} alt="Icon 3" />
+            <p>
+              INFRASTRUCTURE <br /> DEVELOPMENT
+            </p>
+            <p>lorem ipsum</p>
+          </div>
+          <div className="icon">
+            <img src={Icon4} alt="Icon 4" />
+            <p>
+              FAMILY <br /> SUPPORT
+            </p>
+            <p>lorem ipsum</p>
+          </div>
+        </div>
       </div>
-      <div className="column">
-      <div className="fundraising-text">
-        <p>
-          Nawi" highlights the distressing issue of early marriages in Kenya,
-          depicting the challenges faced by young individuals thrust into
-          matrimony. The film explores the interconnection between early
-          marriages and education obstacles, showcasing the profound impact on
-          the lives of those involved. It calls for increased awareness and
-          policy changes to break this cycle, advocating for a future where
-          education becomes a key tool against early marriages.
-        </p>
+      <div className="trailer-section">
+        <h2>TRAILER</h2>
+        
       </div>
-      </div>
-    </div>
     </div>
   );
 };
