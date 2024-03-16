2224d4761d33e6865adf14202e7d2f29dece86e3
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-16 11:02:57 +0300

    Added all changes to Newss

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import image from "../Assets/News.png"; 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <div className="news-container">
+      <img
+        className="image-container"
+        src={image}
+        alt="Woman in traditional attire"
+      />
+      <div className="text-container">
+        <p>Extension of Loropio Girls Senior High School</p>
+        <p>Extension of Loropio Girls Senior High School</p>
+        <p>Extension of Loropio Girls Senior High School</p>
+      </div>
+    </div>
+  );
 };
 
 export default News;
