7029dca441d84ce645e96689c8d0af9fd44cc4c3
joanawuor <joan.obunge@learninglions.org>
2024-03-20 00:38:30 +0300

    contact work in progress

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,39 @@
 import React from "react";
+import Icon10 from "../Assets/icons/icon10.png";
+import Icon11 from "../Assets/icons/icon11.png";
+
+function handleSubmit() {
+  console.log("handleSubmit");
+}
+function handleEmailChange() {
+  console.log("handleEmailChange");
+}
 
 const Contact = () => {
-  return <div>Contact</div>;
+  let email = "";
+  return (
+    <div className="contact-page">
+      <h1 className="contact-title">CONTACT US</h1>
+      <div className="contact-us">
+        <div>
+          <img className="email" src={Icon10} alt="icon 10" />
+          <img className="email" src={Icon11} alt="icon 11" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            aria-label="Your email address"
+            name="email_address"
+            placeholder="Your email address"
+            required
+            type="email"
+            onChange={handleEmailChange}
+            value={email}
+          />
+          <button>SUBSCRIBE</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
